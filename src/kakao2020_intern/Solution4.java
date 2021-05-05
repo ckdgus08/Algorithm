81c48c5345f8ce77e1d014bc3ca47fb9440e3016
ckdgus08 <ckdgus0808@naver.com>
2021-05-05 17:47:55 +0900

    2020 카카오 인턴 4번 문제

--- src/kakao2020_intern/Solution4.java
+++ src/kakao2020_intern/Solution4.java
@@ -1,74 +1,87 @@
 package kakao2020_intern;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class Solution4 {
 
-    boolean[][] visited;
-    int[][] D = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    Queue<Point> q = new LinkedList<>();
+    int[][] D = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    Queue<Point> queue = new LinkedList<>();
+    int result = Integer.MAX_VALUE;
     int N;
-    List<Point> result = new ArrayList<>();
 
     public int solution(int[][] board) {
+
         N = board.length;
-        visited = new boolean[N][N];
 
         bfs(board, 0, 0, N - 1, N - 1);
 
-        return result.stream().map(point -> (point.line * 100 + point.cour * 500)).max(Integer::min).get();
+        return result;
     }
 
-    void bfs(int[][] board, int startX, int startY, int endX, int endY) {
-        visited[startX][startY] = true;
-        q.add(new Point(startX, startY, 0, 0, true));
 
-        while (!q.isEmpty()) {
-            Point now = q.remove();
+    void bfs(int[][] board, int start_x, int start_y, int end_x, int end_y) {
 
-            if (now.x == endX && now.y == endY)
-                result.add(now);
+        queue.add(new Point(start_x, start_y, 0, -1));
+
+        board[start_x][start_y] = 1;
+
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+
+            if (now.x == end_x && now.y == end_y)
+                if (result > now.price)
+                    result = now.price;
 
             for (int i = 0; i < 4; i++) {
-                int newX = now.x + D[i][0];
-                int newY = now.y + D[i][1];
+                int new_x = now.x + D[i][0];
+                int new_y = now.y + D[i][1];
 
-                if (newX < 0 || newY < 0 || newX >= N || newY >= N) continue;
-                if (visited[newX][newY]) continue;
-                if (board[newX][newY] == 1) continue;
-                visited[newX][newY] = true;
+                if (new_x < 0 || new_y < 0 || new_x >= N || new_y >= N) continue;
+                if (board[new_x][new_y] == 1) continue;
 
-                if (now.face) {
-                    if (now.y != newY)
-                        q.add(new Point(newX, newY, now.line + 1, now.cour, true));
-                    else
-                        q.add(new Point(newX, newY, now.line, now.cour + 1, false));
+                int temp_price;
+                int temp_dist;
+                // 세로방향 이동
+                if (now.x == new_x) {
+                    temp_dist = 1;
+                    if (now.dis == -1 || now.dis == 1) {
+                        temp_price = now.price + 100;
+                    } else {
+                        temp_price = now.price + 600;
+                    }
                 } else {
-                    if (now.x != newX)
-                        q.add(new Point(newX, newY, now.line + 1, now.cour, false));
-                    else
-                        q.add(new Point(newX, newY, now.line, now.cour + 1, true));
+                    temp_dist = 2;
+                    if (now.dis == -1 || now.dis == 2) {
+                        temp_price = now.price + 100;
+                    } else {
+                        temp_price = now.price + 600;
+                    }
+                }
+                if (board[new_x][new_y] == 0) {
+                    board[new_x][new_y] = temp_price;
+                    queue.add(new Point(new_x, new_y, temp_price, temp_dist));
+                } else if (board[new_x][new_y] >= temp_price) {
+                    board[new_x][new_y] = temp_price;
+                    queue.add(new Point(new_x, new_y, temp_price, temp_dist));
                 }
             }
         }
     }
 
-    static class Point {
+
+    class Point {
         int x;
         int y;
-        int line;
-        int cour;
-        boolean face;
+        int price;
+        int dis;
 
-        public Point(int x, int y, int line, int cour, boolean face) {
+        public Point(int x, int y, int price, int dis) {
             this.x = x;
             this.y = y;
-            this.line = line;
-            this.cour = cour;
-            this.face = face;
+            this.price = price;
+            this.dis = dis;
         }
     }
+
 }
