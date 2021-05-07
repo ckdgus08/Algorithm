ce0074de1327598035da9e757103a40b21f1b093
ckdgus08 <ckdgus0808@naver.com>
2021-05-07 20:45:54 +0900

    kakao blind 2018 - 비밀지도

---
+++ src/kakao_blind/Solution1.java
@@ -0,0 +1,18 @@
+package kakao_blind;
+
+public class Solution1 {
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++)
+            answer[i] =
+                    String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]))
+                            .replaceAll("1", "#")
+                            .replaceAll("0", " ");
+
+
+        return answer;
+    }
+
+}
