b18be42033b41b0bbb3d6f384869b10bcda9f5e1
ckdgus08 <ckdgus0808@naver.com>
2021-05-17 23:46:29 +0900

    boj 1557 제곱 ㄴㄴ 수

---
+++ src/BOJ/P1557.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1557 {
+
+    static long min;
+    static long max;
+    static boolean[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        min = Long.parseLong(split[0]);
+        max = Long.parseLong(split[1]);
+        arr = new boolean[(int) (max - min + 1)];
+
+        for (long i = 2; i * i <= max; i++) {
+            long pow = i * i;
+            long start = min + (pow - (min % pow)) % pow;
+
+            for (long j = start; j <= max; j += pow) {
+                arr[(int) (j - min)] = true;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i])
+                count++;
+        }
+
+        System.out.print(count);
+    }
+}
